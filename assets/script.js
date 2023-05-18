3cdce5092dd5d1abd8af911774dcc4f7d0ebf01e
WeaselFR <benoit.rogeau@laposte.net>
2023-05-18 11:04:17 +0200

    v10

--- assets/script.js
+++ assets/script.js
@@ -55,6 +55,9 @@ arrowLeft.addEventListener('click', function() {
         image.style.display = "none";
     });
     sliderImages[imageIndex].style.display = "none";
+    // Met à jour le point de balle actif
+    updateSliderDots();
+    console.log('Clic sur la flèche gauche');
 });
 
 // Ajoute un événement de clic pour la flèche droite
@@ -72,16 +75,64 @@ arrowRight.addEventListener('click', function() {
         image.style.display = "none";
     });
     sliderImages[imageIndex].style.display = "none";
+    // Met à jour le point de balle actif
+    updateSliderDots();
+    console.log('Clic sur la flèche droite');
 });
 
-// Dans cet exemple, nous avons sélectionné les flèches "arrow_left" et "arrow_right" à l'aide de la méthode document.querySelector() 
+// Sélectionne les points du slider
+var sliderDots = document.querySelectorAll('.slider-dots li');
+
+// Met à jour l'affichage des points du slider
+function updateSliderDots() {
+  const sliderDots = document.querySelectorAll('.slider-dot');
+  sliderDots.forEach((dot, index) => {
+    if (index === imageIndex) {
+      dot.classList.add('active');
+    } else {
+      dot.classList.remove('active');
+    }
+  });
+}
+
+// Appelle la fonction pour la première image
+updateSliderDots();
+
+// Ajoute un événement de clic pour chaque point du slider
+sliderDots.forEach(function(dot, index) {
+dot.addEventListener('click', function() {
+// Met à jour le compteur d'image avec l'index du point
+imageIndex = index;
+// Met à jour l'image et la tagline dans la div "banner"
+updateBanner();
+// Modifie la source de l'image dans la div "slider"
+sliderImages.forEach(function(image) {
+image.style.display = "none";
+});
+sliderImages[imageIndex].style.display = "block";
+// Met à jour l'affichage des points du slider
+updateSliderDots();
+});
+});
+  
+
+// Dans cet exemple, j'ai sélectionné les flèches "arrow_left" et "arrow_right" à l'aide de la méthode document.querySelector() 
 // et les images du slider à l'aide de la méthode document.querySelectorAll(). 
-// Nous avons ensuite initialisé un compteur d'image imageIndex à 0 pour suivre l'index de l'image actuellement affichée.
-// Dans la fonction de rappel de l'événement de clic de la flèche gauche, nous avons décrémenté le compteur d'image imageIndex, 
-// vérifié si le compteur d'image était inférieur à 0, et si c'est le cas, nous avons ramené le compteur à la dernière image. 
-// Nous avons ensuite modifié la source de l'image dans la div "slider" en utilisant la méthode forEach() pour parcourir chaque image 
+// J'ai ensuite initialisé un compteur d'image imageIndex à 0 pour suivre l'index de l'image actuellement affichée.
+// Dans la fonction de rappel de l'événement de clic de la flèche gauche, j'ai décrémenté le compteur d'image imageIndex, 
+// vérifié si le compteur d'image était inférieur à 0, et si c'est le cas, j'ai ramené le compteur à la dernière image. 
+// J'ai ensuite modifié la source de l'image dans la div "slider" en utilisant la méthode forEach() pour parcourir chaque image 
 // et masquer toutes les images sauf celle avec l'index imageIndex.
 // Dans la fonction de rappel de l'événement de clic de la flèche droite 
-// nous avons incrémenté le compteur d'image imageIndex, vérifié si le compteur d'image dépassait le nombre d'images dans le slider, 
-// et si c'est le cas, nous avons ramené le compteur à la première image. Nous avons ensuite modifié la source de l'image dans la div "slider" en utilisant la méthode forEach() 
+// j'ai incrémenté le compteur d'image imageIndex, vérifié si le compteur d'image dépassait le nombre d'images dans le slider, 
+// et si c'est le cas, j'ai ramené le compteur à la première image. J'ai ensuite modifié la source de l'image dans la div "slider" en utilisant la méthode forEach() 
 // pour parcourir chaque image et masquer toutes les images sauf celle avec l'index imageIndex.
+
+// Comment j'ai défini le premier point comme actif :
+// Tout d'abord, j'ai sélectionné l'élément HTML de la liste à puces où je voulais ajouter les points de balle. Dans ce cas, j'ai sélectionné la liste à puces avec la classe "slider-dots" et j'ai stocké cette sélection dans une variable appelée "sliderDots".
+// J'ai créé une boucle qui a créé un élément "li" pour chaque image dans le slider, en utilisant la méthode "createElement" de JavaScript. J'ai stocké chaque élément "li" dans une variable appelée "dot".
+// J'ai ensuite ajouté une classe "slider-dot" à chaque élément "li" en utilisant la propriété "classList" de JavaScript.
+// J'ai ensuite utilisé la méthode "appendChild" pour ajouter chaque élément "li" créé à la liste à puces "sliderDots".
+// J'ai ajouté un événement "click" à chaque élément "li" en utilisant la méthode "addEventListener". À l'intérieur de la fonction de rappel de l'événement, j'ai mis à jour la variable "imageIndex" avec l'index de l'image correspondante.
+// Enfin, j'ai mis à jour la classe de l'élément "li" correspondant à l'image en cours de visualisation en utilisant la propriété "classList" de JavaScript. J'ai supprimé la classe "active" de tous les éléments "li" et j'ai ajouté la classe "active" uniquement à l'élément "li" correspondant à l'index de l'image en cours de visualisation.
+// Cela a créé les points de balle et a défini le premier point comme actif.
